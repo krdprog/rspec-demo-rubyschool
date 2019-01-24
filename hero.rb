cb354730c476d2564813a3287c2875b4ce9a9502
krdprog <info@krdprog.ru>
2019-01-24 15:37:52 +0300

    Add rspec test and Hero class

--- hero.rb
+++ hero.rb
@@ -1,2 +1,29 @@
 class Hero
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  attr_reader :name
+
+  def power_up
+    @health += 10
+  end
+
+  def power_down
+    @health -= 10
+  end
+
+  def hero_info
+    "У #{@name} есть #{@health} здоровья"
+  end
+
 end
+
+# hero = Hero.new 'foo'
+# puts hero.name
+# puts hero.power_up
+# puts hero.power_down
+# puts her.o.power_down
+# puts hero.hero_info
--- hero_spec.rb
+++ hero_spec.rb
@@ -2,4 +2,10 @@ require './hero'
 
 describe Hero do
 
+  it "has a capitalized name" do
+    hero = Hero.new 'foo'
+
+    expect(hero.name).to eq 'Foo'
+  end
+
 end
